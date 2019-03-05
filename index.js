b4632dd7b9245a567a2ca20745864430b6a97ae6
madishak <33802709+madishak@users.noreply.github.com>
2019-03-05 17:43:40 +0300

    Added files

--- index.js
+++ index.js
@@ -1,91 +1,92 @@
 class SortAndDraw  {
-    constructor(numbersString) {
-        this.nums = numbersString;
-        this.arr;
-        this.l = [];
-        this.newDiv;
-        this.container;
-    }
-
-    getArray()  {
-        this.arr = this.nums.split("").map(element => Number(element));
-        return  this.arr;
-
-    }
-
-    showArray() {
-        return this.drawArray(this.getArray());
+    constructor(numberString) {
+        this.arr = numberString.split("").map(element => Number(element));
+        this.listOfIndexes = [];
+        this.changeColumns = {};
     }
 
     increaseSort() {
-        for (let i = 0; i < this.arr.length - 1; i++) {
-            for (let j = i + 1; j < this.arr.length; j++) {
-                if (this.arr[i] > this.arr[j]) {
-                    [this.arr[i], this.arr[j]] = [this.arr[j], this.arr[i]];
-                    this.l = [i, j, ...this.l];
-                    return this.drawArray(this.arr);
+        let pointer = 1;
+        while (pointer) {
+            pointer = 0;
+
+            for (let i = 0; i < this.arr.length; i++) {
+
+                if (this.arr[i] > this.arr[i + 1]) {
+                    [this.arr[i], this.arr[i + 1]] = [this.arr[i + 1], this.arr[i]];
+                    this.listOfIndexes = [i, i + 1, ...this.listOfIndexes];
+                    this.changeColumns.first = this.arr[i];
+                    this.changeColumns.second = this.arr[i+1];
+                    pointer = 1;
+                    return this.arr;
                 }
             }
         }
-
     }
 
-    backSort()  {
+
+    backSort() {
         for (let i = 0; i < this.arr.length; i++)
         {
-            if (this.l.length != 0) {
-                [this.arr[this.l[0]], this.arr[this.l[1]]] = [this.arr[this.l[1]], this.arr[this.l[0]]];
-                this.l.shift(this.l[0]);
-                this.l.shift(this.l[1]);
-                return this.drawArray(this.arr);
+            if (this.listOfIndexes.length != 0) {
+                [this.arr[this.listOfIndexes[0]], this.arr[this.listOfIndexes[1]]] = [this.arr[this.listOfIndexes[1]], this.arr[this.listOfIndexes[0]]];
+                this.changeColumns.first = this.arr[this.listOfIndexes[0]];
+                this.changeColumns.second = this.arr[this.listOfIndexes[1]];
+                this.listOfIndexes.shift(this.listOfIndexes[0]);
+                this.listOfIndexes.shift(this.listOfIndexes[1]);
+                return this.arr;
             }
         }
-
-
     }
 
 
-    drawArray(arr) {
 
-        this.container = document.createElement('div');
-        this.container.className = "line__inner";
-        document.body.appendChild(this.container);
-        for (let i = 0; i < arr.length; i++) {
 
-            this.newDiv = document.createElement('div');
-            this.newDiv.innerHTML = arr[i];
-            this.newDiv.id = arr[i];
-            this.newDiv.className = "line";
-            this.container.appendChild(this.newDiv);
-            this.newDiv.style.height = 15 * arr[i]+'px';
-            this.newDiv.style.display = 'block';
+
+    drawArray() {
+        let container = document.createElement('div');
+        container.className = "line__inner";
+        document.body.appendChild(container);
+        for (let i = 0; i < this.arr.length; i++) {
+
+            let newDiv = document.createElement('div');
+            newDiv.innerHTML = this.arr[i];
+            newDiv.id = this.arr[i];
+            newDiv.className = "line";
+            container.appendChild(newDiv);
+            newDiv.style.height = 15 * this.arr[i]+'px';
+            newDiv.style.display = 'block';
         }
-        document.body.insertBefore(this.container, h1);
 
-
-        return this.newDiv;
     }
+
+
+    animateArray() {
+        let firstWidth = document.getElementById(this.changeColumns.first).offsetWidth;
+        let secondWidth = document.getElementById(this.changeColumns.second).offsetWidth;
+        let firstColl = document.getElementById(this.changeColumns.first);
+        let secondColl = document.getElementById(this.changeColumns.second);
+        firstColl.style.right = firstWidth + 20 + 'px';
+        secondColl.style.right = -secondWidth - 20 + 'px';
+    }
+
 
 }
 
 
 
+let inputValue = document.getElementById('input').value;
 
-// let inputValue = document.getElementById('input').value;
-
-let sortAndDraw = new SortAndDraw(input.value);
-
+let sortAndDraw = new SortAndDraw(inputValue);
 
 let inputShow = document.getElementById('input');
-inputShow.addEventListener('change', () => sortAndDraw.showArray());
+inputShow.addEventListener('change', () => sortAndDraw.drawArray());
 
 
 let increase = document.getElementById('inc');
-increase.addEventListener('click',() => sortAndDraw.increaseSort());
+increase.addEventListener('click',() => sortAndDraw.animateArray(sortAndDraw.increaseSort()));
 
 
 let decrease = document.getElementById('dec');
-decrease.addEventListener('click', () => sortAndDraw.backSort());
-
-
+decrease.addEventListener('click', () => sortAndDraw.animateArray(sortAndDraw.backSort()));
 
