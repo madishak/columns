b7faf02e5367f3ddd542bbd0e5293b837714c71c
madishak <33802709+madishak@users.noreply.github.com>
2019-04-10 15:00:06 +0300

    Added files

--- index.js
+++ index.js
@@ -1,65 +1,33 @@
-class SortAndDraw  {
+class SortAndDraw {
     constructor(numberString) {
-        this.arr = numberString.split("").map(element => Number(element));
-        this.listOfIndexes = [];
-        this.changeColumns = {};
-        this.newArr = [];
-
+        this.numsArr =  numberString.split("").map(element => Number(element));
+        this.arr = this.numsArr.slice(0);
+        this.storage = [this.numsArr];
+        this.indSaver = {};
 
     }
+    decreaseSort() {
+        this.arr = this.storage.shift();
+        return this.arr;
+    }
 
-    increaseSort() {
-        let pointer = 1;
+    increaseSort()  {
 
-        while (pointer) {
-            pointer = 0;
-
+        let flag = true;
+        while (flag) {
+            flag = false;
             for (let i = 0; i < this.arr.length; i++) {
-
                 if (this.arr[i] > this.arr[i + 1]) {
-                    this.changeColumns.first = this.arr[i];
-                    this.changeColumns.second = this.arr[i+1];
-                    this.newArr = [this.arr, ...this.newArr];
                     [this.arr[i], this.arr[i + 1]] = [this.arr[i + 1], this.arr[i]];
-                    this.listOfIndexes = [i, i + 1, ...this.listOfIndexes];
-
-                    pointer = 1;
+                    this.storage = [this.arr.slice(0), ...this.storage];
+                    flag = true;
                     return this.arr;
                 }
             }
         }
-    }
 
-
-    put() {
-        // let newArr3 = [];
-        // let arr = this.increaseSort();
-        // this.newArr = [arr, ...this.newArr];
-        // newArr3 = [this.increaseSort(), newArr3];
-        // return alert(this.newArr);
-
-        //
-        // this.arrArr = [this.newArr, ...this.arrArr];
-        alert(this.newArr);
 
     }
-
-    backSort() {
-        for (let i = 0; i < this.arr.length; i++)
-        {
-            if (this.listOfIndexes.length != 0) {
-                [this.arr[this.listOfIndexes[0]], this.arr[this.listOfIndexes[1]]] = [this.arr[this.listOfIndexes[1]], this.arr[this.listOfIndexes[0]]];
-                this.changeColumns.first = this.arr[this.listOfIndexes[0]];
-                this.changeColumns.second = this.arr[this.listOfIndexes[1]];
-                this.listOfIndexes.shift(this.listOfIndexes[0]);
-                this.listOfIndexes.shift(this.listOfIndexes[1]);
-                return this.arr;
-            }
-        }
-    }
-
-
-
 
 
     drawArray() {
@@ -80,68 +48,71 @@ class SortAndDraw  {
     }
 
 
-    animateArray() {
-        let firstWidth = document.getElementById(this.changeColumns.first).offsetWidth;
-        let secondWidth = document.getElementById(this.changeColumns.second).offsetWidth;
-        let firstColl = document.getElementById(this.changeColumns.first);
-        let secondColl = document.getElementById(this.changeColumns.second);
-        firstColl.style.right = firstWidth + 20 + 'px';
-        secondColl.style.right = -secondWidth - 20 + 'px';
+    changeNums() {
+
+        if (this.storage.length > 1) {
+            let firstArr  = this.storage[0];
+            let secondArr = this.storage[1];
+            for (let i = 0; i < firstArr.length; i++) {
+                this.indSaver.a1 = firstArr[i];
+                this.indSaver.a2 = firstArr[i+1];
+
+
+
+                for (let j = 0; j < secondArr.length; j++) {
+                    this.indSaver.b1 = secondArr[j];
+                    this.indSaver.b2 = secondArr[j+1];
+
+                    if (this.indSaver.a1 === this.indSaver.b2 && this.indSaver.a2 === this.indSaver.b1)  {
+                        return this.indSaver;
+                    }
+
+
+                }
+
+            }
+
+
+        }
+        else  {
+            return "Nothing";
+        }
     }
 
-    anArray() {
-        let firstWidth = document.getElementById(this.changeColumns.first).offsetWidth;
-        let secondWidth = document.getElementById(this.changeColumns.second).offsetWidth;
-        let firstColl = document.getElementById(this.changeColumns.first);
-        let secondColl = document.getElementById(this.changeColumns.second);
-        firstColl.style.right = secondWidth - 40 + 'px';
-        secondColl.style.right = -firstWidth + 40 + 'px';
+    animate() {
+
+        let firstColl = document.getElementById(this.indSaver.a2);
+        let secondColl = document.getElementById(this.indSaver.a1);
+
+
+        for(let i = 0; i < this.arr.length; i++) {
+
+            firstColl.style.left = i + 28 + 'px';
+            secondColl.style.left = i - 28 + 'px';
+
+
+        }
     }
 
-    animArray() {
-        // let firstWidth = document.getElementById(this.changeColumns.first).offsetWidth;
-        // let secondWidth = document.getElementById(this.changeColumns.second).offsetWidth;
-        let firstColl = document.getElementById(this.changeColumns.first);
-        let secondColl = document.getElementById(this.changeColumns.second);
-        firstColl.className = 'line' + 'move_right';
-        secondColl.className = 'line' + 'move_left';
-        firstColl.style.right = 20 + 'px';
-        secondColl.style.left = 20 + 'px';
-        // for (let i = 0; i < this.arr.length; i++) {
-        //     if (this.arr[i] == this.changeColumns.first) {
-        //         firstColl.style.right = this.arr[i] + 7 + 20 + 'px';
-        //     }
-        //     if (this.arr[i] == this.changeColumns.second) {
-        //         secondColl.style.right = this.arr[i] - 7 - 20 + 'px';
-        //     }
-        //     // firstColl.style.right = this.arr[i] + 7 + 20 + 'px';
-        //     //secondColl.style.right = this.arr[i-1] - 7 - 20 + 'px';
-        // }
-
-        //secondColl.style.right = -secondWidth - 20 + 'px';
-    }
 
 
 }
 
-
-
 let inputValue = document.getElementById('input').value;
 
 let sortAndDraw = new SortAndDraw(inputValue);
+
 
 let inputShow = document.getElementById('input');
 inputShow.addEventListener('change', () => sortAndDraw.drawArray());
 
 
 let increase = document.getElementById('inc');
-increase.addEventListener('click',() => sortAndDraw.anArray(sortAndDraw.increaseSort()));
+increase.addEventListener('click',() => sortAndDraw.animate(sortAndDraw.changeNums(sortAndDraw.increaseSort())));
+
 
 
 let decrease = document.getElementById('dec');
-decrease.addEventListener('click', () => sortAndDraw.drawArray(sortAndDraw.backSort()));
+decrease.addEventListener('click', () => sortAndDraw.animate(sortAndDraw.changeNums(sortAndDraw.decreaseSort())));
 
-
-let ob = document.getElementById('ob');
-ob.addEventListener('click', () => sortAndDraw.put());
 
