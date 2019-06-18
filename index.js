7df7f9575f1b6527f299e281717fee35e9efe5a7
madishak <33802709+madishak@users.noreply.github.com>
2019-06-18 17:55:08 +0300

    Animated bubble sort

--- index.js
+++ index.js
@@ -1,139 +1,143 @@
 class Sort {
     constructor(numberString) {
-        this.numsArr =  numberString.split("").map(element => Number(element));
+        this.numsArr = numberString.split("").map(element => Number(element));
         this.arr = this.numsArr.slice(0);
         this.listOfIndexes = [];
-        this.struct = {"arr": this.arr}
-    }
-
-    arrayAndIndexes(){
-        return this.struct;
     }
 
 
     decreaseSort() {
-        for (let i = 0; i < this.arr.length; i++)
-        {
+        for (let i = 0; i < this.arr.length; i++) {
             if (this.listOfIndexes.length !== 0) {
                 [this.arr[this.listOfIndexes[0]], this.arr[this.listOfIndexes[1]]] = [this.arr[this.listOfIndexes[1]], this.arr[this.listOfIndexes[0]]];
                 this.listOfIndexes.shift(this.listOfIndexes[0]);
                 this.listOfIndexes.shift(this.listOfIndexes[1]);
 
-                this.struct.indexes = this.listOfIndexes;
-                this.struct.arr = this.arr;
-
-
                 return this.arr;
             }
+
         }
+        return this.arr;
     }
 
 
+    increaseSort() {
 
-    increaseSort()  {
+        // let flag = true;
+        // while (flag) {
+        //     flag = false;
 
-        let flag = true;
-        while (flag) {
-            flag = false;
-            for (let i = 0; i < this.arr.length; i++) {
-                if (this.arr[i] > this.arr[i + 1]) {
-                    [this.arr[i], this.arr[i + 1]] = [this.arr[i + 1], this.arr[i]];
-                    this.listOfIndexes = [i + 1, i, ...this.listOfIndexes];
-                    flag = true;
-
-                    this.struct.indexes = this.listOfIndexes;
-                    this.struct.arr = this.arr;
-
-
-                    return this.arr;
+            for (let i = 0; i < this.arr.length-1; i++) {
+                for (let j = 0; j < this.arr.length-1-i; j++) {
+                    if (this.arr[j+1] < this.arr[j]) {
+                        [this.arr[j+1], this.arr[j]] = [this.arr[j], this.arr[j+1]];
+                        this.listOfIndexes = [j, j + 1, ...this.listOfIndexes];
+                        // flag = true;
+                        return this.arr;
+                    }
                 }
             }
-        }
-
+        // }
+        return this.arr;
 
     }
-
-
-
 
 
 }
 
 class Draw {
-    constructor(sequence) {
-        this.arrayAndIndexes = sequence;
-        this.arr = this.arrayAndIndexes.arr;
+    constructor(array) {
+        this.arr = array;
+        this.columnIndexArr = [];
 
     }
 
 
-
     drawArray() {
-        const fixedColumnHeight = 15;
-        const columnMargin = 5;
+
+        const FIXED_COLUMN_HEIGHT = 15;
+
+        const OFFSET = 30;
+
         let container = document.createElement('div');
         container.className = "line__inner";
         container.id = "line__inner";
-        document.body.appendChild(container);
+
+
         for (let i = 0; i < this.arr.length; i++) {
 
             let newDiv = document.createElement('div');
             newDiv.innerHTML = this.arr[i];
             newDiv.id = this.arr[i];
             newDiv.className = "line";
+            this.columnIndexArr.push(i);
+            newDiv.style.height = `${FIXED_COLUMN_HEIGHT * this.arr[i]}px`;
+            newDiv.style.left =  `${i * OFFSET}px`;
             container.appendChild(newDiv);
-            newDiv.style.height = fixedColumnHeight * this.arr[i]+'px';
-            newDiv.style.left = i * columnMargin + 'px';
-            newDiv.style.display = 'block';
         }
-
+        document.body.appendChild(container);
     }
 
-    movement() {
-        let listOfIndexes = this.arrayAndIndexes.indexes;   //indexes' stack (sort steps)
+
+    movement(newArr) {
+
+        const bg = {
+            first : 0,
+            second : 1
+        };
+
+
+        const COLUMN_MARGIN = 30;
 
         const [...columns] = document.getElementsByClassName('line'); //columns => HTML objects
 
-        const leftWidths = columns.reduce((arr, elem) => [elem.style.left, ...arr], []).reverse();     //columns' left positions
 
-        let copyLeftWidth = leftWidths.slice(0); //leftWidths array's copy
+        for (let i = 0; i < newArr.length; i++) {
+            if (newArr[i] !== this.arr[i]) {
+                [this.columnIndexArr[i], this.columnIndexArr[i+1]] = [this.columnIndexArr[i+1], this.columnIndexArr[i]];
+                bg.first = this.columnIndexArr[i];
+                bg.second = this.columnIndexArr[i+1];
 
-        for (let i = 0; i < columns.length; i++) {
-            for (let j = 0; j < listOfIndexes.length; j++) {
-                for (let k = 0; k < leftWidths.length; k++) {
-                    if (listOfIndexes[j] === k) {
-                        columns[listOfIndexes[j]].style.left = leftWidths[k];
-                       // columns[listOfIndexes[j]].style.left = columns[i] * 10 + 'px';
-
-                    }
-                }
+                break;
             }
         }
+        for (let i = 0; i < columns.length; i++) {
+            columns[this.columnIndexArr[i]].style.left = `${i * COLUMN_MARGIN}px`;
+            columns[this.columnIndexArr[i]].style.backgroundColor = 'dodgerblue';
+            columns[bg.first].style.backgroundColor = 'red';
+            columns[bg.second].style.backgroundColor = 'red';
 
 
+        }
+        this.arr = [...newArr];// плохо
 
     }
 
 
 }
 
-let inputValue = document.getElementById('input').value;
 
-let sort = new Sort(inputValue);
-
-let draw = new Draw(sort.arrayAndIndexes());
-
-let inputShow = document.getElementById('input');
-inputShow.addEventListener('change', () => draw.drawArray());
+const inputShow = document.getElementById('input');
+inputShow.addEventListener('change', () => renderCollection());
 
 
-let increase = document.getElementById('inc');
-increase.addEventListener('click',() => draw.movement(sort.increaseSort()));
+const renderCollection = () => {
+    let inputValue = document.querySelector('#input').value;
+    console.log(inputValue);
+
+    let sort = new Sort(inputValue);
+
+    let draw = new Draw(sort.arr);
 
 
-
-let decrease = document.getElementById('dec');
-decrease.addEventListener('click', () => draw.drawArray(sort.decreaseSort()));
-
+    draw.movement(sort.arr);
+    draw.drawArray();
 
 
+    let decrease = document.getElementById('dec');
+    decrease.addEventListener('click', () => draw.movement(sort.decreaseSort()));
+
+    let increase = document.getElementById('inc');
+    increase.addEventListener('click', () => draw.movement(sort.increaseSort()));
+
+};
