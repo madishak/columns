ec0ab6056c608d9a986b524aaff19bf973e80ea8
madishak <33802709+madishak@users.noreply.github.com>
2019-07-18 17:06:50 +0300

    Added files

--- sort.js
+++ sort.js
@@ -29,42 +29,59 @@ class Sort {
         // while (flag) {
         //     flag = false;
         let t;
-            for (let i = 0; i < this.arr.length-1; i++) {
-                for (let j = 0; j < this.arr.length-1; j++) {
+            for (let i = 0; i < this.arr.length; i++) {
+                for (let j = 0; j < this.arr.length-i-1; j++) {
                     if (this.arr[j] > this.arr[j+1]) {
                         [this.arr[j], this.arr[j+1]] = [this.arr[j+1], this.arr[j]];
-                        console.log(j,j+1);
+                        //console.log(j,j+1);
                         this.listOfIndexes = [j, j+1, ...this.listOfIndexes];
-                        this.listOfIndexesNext = this.listOfIndexes.reverse();
+                        this.listOfIndexesNext = [...this.listOfIndexesNext, j, j+1];
                         // flag = true;
                         //continue;
                         //return this.arr;
-                        t = this.arr;
+                        //return  this.arr;
                     }
                 }
 
             }
         // }
-        t = this.arr;
+        return  this.arr;
         //     console.log(this.listOfIndexes);
         // console.log(this.listOfIndexes.reverse());
         // console.log(this.arr);
-        return t;
+        //return t;
 
     }
 
     change() {
-        const listOfIndexes = this.listOfIndexes.reverse();
-        console.log(this.listOfIndexesNext);
-        for (let i = 0; i <this.listOfIndexesNext.length; i++) {
-            for (let j = 0; j < this.arr.length; j++) {
-                if (this.listOfIndexesNext.length !== 0) {
-                    [this.arr[this.listOfIndexesNext[i]], this.arr[this.listOfIndexesNext[i+1]]] = [this.arr[this.listOfIndexesNext[i+1]], this.arr[this.listOfIndexesNext[i]]];
-                    this.listOfIndexesNext.shift(this.listOfIndexesNext[0]);
-                    this.listOfIndexesNext.shift(this.listOfIndexesNext[1]);
-                    return this.arr;
-                }
-            }
+        let listOfIndexes = this.listOfIndexes.slice(0);
+        console.log(this.arr);
+        console.log(listOfIndexes);
+
+        for (let j = 0; j < this.arr.length; j++) {
+
+        // for (let i = 2; i < this.listOfIndexesNext.length-1; i++) {
+        //     for (let k = i+1; k < this.listOfIndexesNext.length; k++) {
+            //let a = this.listOfIndexesNext[i];
+
+
+
+                    if (this.listOfIndexesNext.length !== 0) {
+                        [this.arr[listOfIndexes[0]], this.arr[listOfIndexes[1]]] = [this.arr[listOfIndexes[1]], this.arr[listOfIndexes[0]]];
+                        listOfIndexes.shift(listOfIndexes[0]);
+                        listOfIndexes.shift(listOfIndexes[1]);
+                        // break;
+                        //console.log(i, k);
+                        // i = i+2;
+                        // k = k+2;
+
+                        console.log(this.arr);
+                        return this.arr;
+
+                    }
+            //     }
+            // }
+
         }
         // console.log(this.newArr);
         // for (let i = 0; i < this.arr.length; i++) {
